0bf118e743b4ffdfa9a998c77f3aeee4c274efd7
Your Name <you@example.com>
2026-05-06 14:32:38 +0200

    boilerplate

---
+++ main.py
@@ -0,0 +1,10 @@
+from docxtpl import DocxTemplate
+import json
+
+doc = DocxTemplate("./template/cv_template.docx")
+
+with open("./data/cv.json") as f:
+    context = json.load(f)
+
+doc.render(context)
+doc.save("./data/output_cv.docx")
